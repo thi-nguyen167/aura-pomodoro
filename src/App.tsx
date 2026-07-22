b9ec519f03c0d7871380aef6dfe155c27de789cc
thi-nguyen167 <athi.nguyen167@gmail.com>
2026-07-22 12:57:03 -0600

    feat: Implement header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <>
-      <div className="">React</div>
-    </>
+    <body className="w-full h-full font-sans text-base text-on-background bg-background overflow-hidden flex flex-col">
+      <Header />
+    </body>
   );
 }
 
