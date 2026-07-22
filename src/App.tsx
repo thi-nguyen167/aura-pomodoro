143840e4bb3aab7512e4de5fba86208a023c67a3
thi-nguyen167 <athi.nguyen167@gmail.com>
2026-07-22 14:42:10 -0600

    feat: Implement mixer col

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,15 @@ import Task from "./components/Task";
 
 function App() {
   return (
-    <body className="w-full h-full font-sans text-base text-on-background bg-background overflow-hidden flex flex-col">
+    <div className="w-screen h-screen font-sans text-base text-on-background bg-background flex flex-col overflow-hidden">
       <Header />
 
-      <main className="relative z-10 flex-1 grid grid-cols-1 lg:grid-cols-[1fr_1.5fr_1fr] gap-gutter p-container min-h-screen">
+      <main className="relative z-10 flex-1 grid grid-cols-1 lg:grid-cols-[1fr_1.5fr_1fr] gap-gutter p-container min-h-0">
         <MixerDrawer />
         <Timer />
         <Task />
       </main>
-    </body>
+    </div>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,9 @@ import { SlidersHorizontal } from "lucide-react";
 const Header = () => {
   return (
     <>
-      <header className="relative z-10 px-sm lg:px-container py-4 lg:py-8 flex shrink-0">
+      <header className="relative z-10 pt-container px-container pb-0 flex shrink-0">
         <div className="flex justify-between items-center w-full">
-          <h1 className="font-mono text-6xl font-extrabold text-primary text-shadow-glow">
+          <h1 className="font-mono text-5xl font-extrabold text-primary text-shadow-glow">
             Aura <br />{" "}
             <span className="font-sans text-2xl uppercase tracking-[0.2em] text-primary-container">
               Pomodoro
