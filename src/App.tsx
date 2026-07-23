735fb67ee3d5aeb84a236bc1db8dbb025fadc3eb
thi-nguyen167 <athi.nguyen167@gmail.com>
2026-07-22 19:43:13 -0600

    feat: add tsparticles background integration

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import Header from "./components/Header";
 import MixerDrawer from "./components/MixerDrawer";
 import Timer from "./components/Timer";
 import Task from "./components/Task";
+import ParticleBackground from "./components/ParticleBackground";
 
 function App() {
   return (
     <div className="w-screen h-screen font-sans text-base text-on-background bg-background flex flex-col overflow-hidden">
+      <ParticleBackground />
       <Header />
 
       <main className="relative z-10 flex-1 grid grid-cols-1 lg:grid-cols-[1fr_1.5fr_1fr] gap-gutter p-container min-h-0">
